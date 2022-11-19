b0083ca1f5169bfdefe46acdb640614ed825ff40
Wanderson M <78830069+wanderson-os@users.noreply.github.com>
2022-11-19 19:10:50 -0300

    Update arquivos_delete.py

--- delete/arquivos_delete.py
+++ delete/arquivos_delete.py
@@ -2,9 +2,7 @@ import os
 from resourcess.env import UPLOAD_FOLDER
 from flask_restful import Resource, request
 
-
 class Delete(Resource):
-
   def delete(self, nome):
     arquivo_delete = ""
     for arq in os.listdir(UPLOAD_FOLDER):
@@ -12,6 +10,5 @@ class Delete(Resource):
         arquivo_delete = arq
       if arquivo_delete:
         os.remove(UPLOAD_FOLDER + "/" + nome)
-
         return "Aquivo removido com sucesso!", 200
     return "Arquivo não encontrado", 500
